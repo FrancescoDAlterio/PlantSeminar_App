81fabd4069aeb0bc01ef7ea8ed00485c76c79872
FrancescoDAlterio <francesco.dalterio1992@gmail.com>
2018-07-08 18:10:43 +0200

    work in progress...

---
+++ app/src/main/java/com/example/francesco/plantseminar_app/connection/Server_Constants.java
@@ -0,0 +1,24 @@
+package com.example.francesco.plantseminar_app.connection;
+
+public class Server_Constants {
+
+    public static final String HOST = "192.168.1.14";
+    public static final String PORT = ":4567";
+    public static final String passwordSSLKey="lavitatua";
+
+
+    //HTTP TAG
+    //todo: da cambiare!
+    public static final String REQ_TAG = "PSBackend Request";
+
+    //ERROR MESSAGES
+    public static final String error_code = "code";
+    public static final String error_message = "message";
+
+
+    //water valve
+
+    public static final String water_configuration = "type";
+
+
+}
